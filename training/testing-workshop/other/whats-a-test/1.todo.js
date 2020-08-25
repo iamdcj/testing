9066dc3ea246df9c6f1338a0f66c8be47763bcde
David Christian Jones <david@dcj.codes>
2020-08-25 12:58:05 -0400

    initial exercise

--- training/testing-workshop/other/whats-a-test/1.todo.js
+++ training/testing-workshop/other/whats-a-test/1.todo.js
@@ -21,3 +21,17 @@ error if the subtract function were to have a bug
 // sum is intentionally broken so you can see errors in the tests
 const sum = (a, b) => a - b
 const subtract = (a, b) => a - b
+
+const testSum = sum(1, 2);
+if(testSum === 3) {
+  console.log('✅')
+} else {
+  throw Error(`Expected 3, returned ${testSum}`)
+}
+
+const testSubtract = subtract(2, 1);
+if(testSubtract === 1) {
+  console.log('✅')
+} else {
+  throw Error(`Expected 1, returned ${testSubtract}`)
+}
