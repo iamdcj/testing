484196ee3f35a211632780bfd4a7f19dfb407652
David Christian Jones <david@dcj.codes>
2020-08-25 13:14:34 -0400

    assertion lib progress

--- training/testing-workshop/other/whats-a-test/2.todo.js
+++ training/testing-workshop/other/whats-a-test/2.todo.js
@@ -1,32 +1,16 @@
-// writing an assertion library
-/*
-
-Now let's implement our own assertion library.
-Create a function called `expect` that accepts an "actual"
-and returns an object of assertions.
-
-Tip: I want to be able to use it like so:
-
-> expect(actual).toBe(expected)
-
-Then run this code with `node 2.todo`
-
-> Make sure you're in the right directory!
-
- */
-
 const {sum, subtract} = require('./math')
 
-let result, expected
-
-result = sum(3, 7)
-expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+function expect(result) {
+  return {
+    toBe(expected) {
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`)
+      } else {
+        console.log('✅ All good!')
+      }
+    }
+  }
 }
 
-result = subtract(7, 3)
-expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(sum(3, 7)).toBe(10);
+expect(subtract(7, 3)).toBe(4);
