0c7bf6082edd7307e155061747a30ee0a92c6d7f
David Christian Jones <david@dcj.codes>
2020-08-30 18:24:14 -0400

    tidy training

--- training/testing-workshop/other/whats-a-test/3.todo.js
+++ training/testing-workshop/other/whats-a-test/3.todo.js
@@ -1,41 +1,13 @@
-// writing a testing framework
-/*
-
-Let's improve our error messages a bit, create a function
-called `test` which can be given a title and a callback.
-
-Then add a try/catch so you can log the title with
-a note of success or error.
-
-Then wrap each of your tests in a `test` function.
-
-This also means that we can run all the tests even
-if one of them fails!
-
-Example of test function
-test(title, () => {
-  // arrange
-  // act
-  // assert
-}
-
-Then run this code with `node 3.todo`
-
-> Make sure you're in the right directory!
-
- */
-
 const {sum, subtract} = require('./math')
 
-let result, expected
-
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
-
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+function test(title, callback) {
+  try {
+    callback();
+    console.log('✅', title )
+  } catch (error) {
+    console.error('❌', title, error.message)
+  }
+}
 
 function expect(actual) {
   return {
@@ -46,3 +18,17 @@ function expect(actual) {
     },
   }
 }
+
+test('subtract', () => {
+  result = subtract(7, 3)
+  expected = 4
+
+  expect(result).toBe(expected)
+})
+
+test('sum', () => {
+  result = sum(3, 7)
+  expected = 10
+
+  expect(result).toBe(expected)
+})
